71c0ab4b66e6b9bf986f22f3cfad71f982eeec72
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-09-10 18:22:17 +0530

    Fix type
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- ui/src/store/resources.ts
+++ ui/src/store/resources.ts
@@ -6,140 +6,137 @@ import { ICatalogStore } from "./catalog";
 import fuzzysort from "fuzzysort";
 
 const Catalog = types.model("Catalog", {
-  id: types.number,
-  type: types.string
+	id: types.number,
+	type: types.string,
 });
 
 const ResourceVersion = types.model("Version", {
-  id: types.number,
-  version: types.optional(types.string, "0.1"),
-  displayName: types.string,
-  description: types.string,
-  minPipelinesVersion: types.optional(types.string, ""),
-  rawURL: types.string,
-  webURL: types.string,
-  updatedAt: types.string
+	id: types.number,
+	version: types.optional(types.string, "0.1"),
+	displayName: types.string,
+	description: types.string,
+	minPipelinesVersion: types.optional(types.string, ""),
+	rawURL: types.string,
+	webURL: types.string,
+	updatedAt: types.string,
 });
 
 export const Resource = types.model("Resource", {
-  id: types.number,
-  kind: types.string,
-  name: types.string,
-  catalog: Catalog,
-  latestVersion: ResourceVersion,
-  tags: types.optional(types.array(Tag), []),
-  rating: types.optional(types.number, 0)
+	id: types.number,
+	kind: types.string,
+	name: types.string,
+	catalog: Catalog,
+	latestVersion: ResourceVersion,
+	tags: types.optional(types.array(Tag), []),
+	rating: types.optional(types.number, 0),
 });
 
 export type IResource = Instance<typeof Resource>;
 
 export const ResourceStore = types
-  .model("ResourceStore", {
-    resources: types.array(Resource),
-    isLoading: true,
-    searchText: types.optional(types.string, ""),
-    err: ""
-  })
+	.model("ResourceStore", {
+		resources: types.array(Resource),
+		isLoading: true,
+		searchText: types.optional(types.string, ""),
+		err: "",
+	})
 
-  .views(self => ({
-    get api(): Api {
-      return getEnv(self).api;
-    },
-    get kindStore(): IKindStore {
-      return getEnv(self).kindStore;
-    },
-    get catalogStore(): ICatalogStore {
-      return getEnv(self).catalogStore;
-    },
-    get categoryStore(): ICategoryStore {
-      return getEnv(self).categoryStore;
-    },
-    get count() {
-      return self.resources.length;
-    },
-    get searchResource() {
-      const { searchText } = self;
-      return searchText;
-    },
-    get resourceList() {
-      const { resources } = self;
-      return resources;
-    }
-  }))
+	.views((self) => ({
+		get api(): Api {
+			return getEnv(self).api;
+		},
+		get kindStore(): IKindStore {
+			return getEnv(self).kindStore;
+		},
+		get catalogStore(): ICatalogStore {
+			return getEnv(self).catalogStore;
+		},
+		get categoryStore(): ICategoryStore {
+			return getEnv(self).categoryStore;
+		},
+		get count() {
+			return self.resources.length;
+		},
+		get searchResource() {
+			const { searchText } = self;
+			return searchText;
+		},
+		get resourceList() {
+			const { resources } = self;
+			return resources;
+		},
+	}))
 
-  .actions(self => ({
-    add(item: IResource) {
-      self.resources.push(item);
-    },
-    setLoading(l: boolean) {
-      self.isLoading = l;
-    },
-    search(text: string) {
-      self.searchText = text;
-    }
-  }))
+	.actions((self) => ({
+		add(item: IResource) {
+			self.resources.push(item);
+		},
+		setLoading(l: boolean) {
+			self.isLoading = l;
+		},
+		search(text: string) {
+			self.searchText = text;
+		},
+	}))
 
-  .actions(self => ({
-    load: flow(function*() {
-      const catalogItem: any = new Set();
-      const kindItem: any = new Set();
-      try {
-        self.setLoading(true);
+	.actions((self) => ({
+		load: flow(function* () {
+			const catalogItem = new Set();
+			const kindItem = new Set();
+			try {
+				self.setLoading(true);
 
-        const { api } = self;
+				const { api } = self;
 
-        const json = yield api.resources();
-        json.data.forEach((item: IResource) => {
-          self.add(item);
-          catalogItem.add(item.catalog.type);
-          kindItem.add(item.kind);
-        });
+				const json = yield api.resources();
+				json.data.forEach((item: IResource) => {
+					self.add(item);
+					catalogItem.add(item.catalog.type);
+					kindItem.add(item.kind);
+				});
 
-        [...kindItem].forEach((kindName: string) => {
-          self.kindStore.add({ name: kindName });
-        });
+				[...Array.from(kindItem)].forEach((c) => {
+					self.kindStore.add({ name: c });
+				});
 
-        [...catalogItem].forEach((catalogName: string) => {
-          self.catalogStore.add({ name: catalogName });
-        });
-      } catch (err) {
-        self.err = err.toString();
-      }
-      self.setLoading(false);
-    })
-  }))
+				[...Array.from(catalogItem)].forEach((c) => {
+					self.catalogStore.add({ name: c });
+				});
+			} catch (err) {
+				self.err = err.toString();
+			}
+			self.setLoading(false);
+		}),
+	}))
 
-  .views(self => ({
-    get list() {
-      const { resources } = self;
-      const { searchText } = self;
+	.views((self) => ({
+		get list() {
+			const { resources } = self;
+			const { searchText } = self;
 
-      const kind = new Set(self.kindStore.selectedkind);
-      const tag = new Set(self.categoryStore.tags);
-      const catalog = new Set(self.catalogStore.selectedCatalogType);
+			const kind = new Set(self.kindStore.selectedkind);
+			const tag = new Set(self.categoryStore.tags);
+			const catalog = new Set(self.catalogStore.selectedCatalogType);
 
-      const filterResources = resources.filter((r: any) =>
-        (tag.size > 0
-          ? r.tags.some((t: any) => tag.has(t.name))
-          : true) && (catalog.size > 0
-          ? catalog.has(r.catalog.type)
-          : true) && (kind.size > 0
-          ? kind.has(r.kind)
-          : true)
-      );
-      return searchText !== ""
-        ? fuzzysort
-            .go(searchText, filterResources, {
-              keys: ["name", "displayName"]
-            })
-            .map((resource: any) => resource.obj)
-        : filterResources;
-    }
-  }))
+			const filterResources = resources.filter(
+				(r: any) =>
+					(tag.size > 0 ? r.tags.some((t: any) => tag.has(t.name)) : true) &&
+					(catalog.size > 0 ? catalog.has(r.catalog.type) : true) &&
+					(kind.size > 0 ? kind.has(r.kind) : true)
+			);
+			return searchText !== ""
+				? fuzzysort
+						.go(searchText, filterResources, {
+							keys: ["name", "displayName"],
+						})
+						.map((resource: any) => resource.obj)
+				: filterResources;
+		},
+	}))
 
-  .actions(self => ({
-    afterCreate() {
-      self.resources.clear();
-      self.load();
-    }
-  }));
+	.actions((self) => ({
+		afterCreate() {
+			self.resources.clear();
+			self.load();
+		},
+	}));
