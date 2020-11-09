c211283663967494998c9c9d22dbba314df92cdb
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-11-09 20:38:29 +0530

    Details page for resources
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- ui/src/components/Details/Details.tsx
+++ ui/src/components/Details/Details.tsx
@@ -1,11 +1,71 @@
 import React from 'react';
-import { GridItem } from '@patternfly/react-core';
+import { Grid, Card, Tabs, Tab, GridItem, CardHeader } from '@patternfly/react-core';
+import ReactMarkDown from 'react-markdown';
+import CodeBlock from './CodeBlock';
+import './Details.css';
 
 const Details: React.FC = () => {
+  const [readme, setReadme] = React.useState('');
+
+  const [activeTabKey, setActiveTabKey] = React.useState(0);
+  const handleTabClick = (event: any, tabIndex: any) => {
+    setActiveTabKey(tabIndex);
+  };
+
+  React.useEffect(() => {
+    fetch('https://raw.githubusercontent.com/tektoncd/catalog/master/task/az/0.1/README.md')
+      .then((res) => res.text())
+      .then((data) => setReadme(data));
+  });
+
   return (
-    <React.Fragment>
-      <GridItem span={12}>Add Resources detail here</GridItem>
-    </React.Fragment>
+    <Grid style={{ maxWidth: '65em', margin: 'auto' }}>
+      <GridItem span={12}>
+        <Card>
+          <CardHeader style={{ paddingTop: '2em' }}>
+            <Grid style={{ width: '90em' }}>
+              <GridItem span={12}>
+                <Tabs
+                  activeKey={activeTabKey}
+                  isSecondary
+                  onSelect={handleTabClick}
+                  style={{ boxShadow: 'none' }}
+                >
+                  <Tab eventKey={0} title="Description" style={{ backgroundColor: 'white' }}>
+                    <hr
+                      style={{
+                        backgroundColor: '#EDEDED',
+                        marginBottom: '1em'
+                      }}
+                    ></hr>
+                    <ReactMarkDown
+                      source={readme}
+                      escapeHtml={true}
+                      renderers={{ code: CodeBlock }}
+                      className="readme"
+                    />
+                  </Tab>
+                  <Tab eventKey={1} title="YAML" style={{ backgroundColor: 'white' }}>
+                    <hr
+                      style={{
+                        backgroundColor: '#EDEDED',
+                        marginBottom: '1em'
+                      }}
+                    ></hr>
+                    <ReactMarkDown
+                      source={readme}
+                      escapeHtml={true}
+                      renderers={{ code: CodeBlock }}
+                      className="yaml"
+                    />
+                  </Tab>
+                </Tabs>
+              </GridItem>
+            </Grid>
+          </CardHeader>
+        </Card>
+      </GridItem>
+    </Grid>
   );
 };
 export default Details;
