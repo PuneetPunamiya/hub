eb496253c952bd4529a12470549defa7180e09af
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-11-18 14:19:59 +0530

    Adds layout for Homepage
      This patch includes following component to create Homepage layout
        - Header
        - Footer
        - SearchBar
        - Left Pane to Filter Resources
        - Resources for rendering resources
        - Details for rendering resource detail
        - BackGround for Homepage banner
    
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- ui/src/containers/App/index.tsx
+++ ui/src/containers/App/index.tsx
@@ -3,20 +3,40 @@ import { observer } from 'mobx-react';
 import '@patternfly/react-core/dist/styles/base.css';
 import { createProvider } from '../../store/root';
 import LeftPane from '../../components/LeftPane';
-import { Grid, GridItem } from '@patternfly/react-core';
+import { Grid, GridItem, Page, PageSection } from '@patternfly/react-core';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Background from '../Background';
+import Header from '../Header';
+import Footer from '../Footer';
+import Resources from '../Resources';
+import Details from '../Details';
+import './App.css';
 
 const Provider = createProvider();
 
 const App: React.FC = observer(() => {
   return (
     <Provider>
-      <div className="App">
-        <Grid>
-          <GridItem span={2}>
-            <LeftPane />
-          </GridItem>
-        </Grid>
-      </div>
+      <Router>
+        <Page header={<Header />}>
+          <Route exact path="/" component={Background} />
+          <PageSection>
+            <Grid hasGutter>
+              <Grid>
+                <Route exact path="/resource/name" component={Details}></Route>
+              </Grid>
+              <GridItem span={2} rowSpan={1}>
+                <Route exact path="/" component={() => <LeftPane />}></Route>
+              </GridItem>
+
+              <GridItem span={10} rowSpan={1}>
+                <Route exact path="/" component={Resources}></Route>
+              </GridItem>
+            </Grid>
+          </PageSection>
+          <Footer />
+        </Page>
+      </Router>
     </Provider>
   );
 });
