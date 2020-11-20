a17b404808f111b5b120b8ba60f04fd1e3e7a7cf
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-11-20 10:27:21 +0530

    Add resources on home page
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- ui/src/containers/Resources/index.tsx
+++ ui/src/containers/Resources/index.tsx
@@ -6,10 +6,29 @@ import {
   CardBody,
   Pagination,
   GridItem,
-  Grid
+  Grid,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+  TextContent,
+  Badge,
+  CardActions
 } from '@patternfly/react-core';
+import { BuildIcon, UserIcon, StarIcon } from '@patternfly/react-icons';
 import { Link } from 'react-router-dom';
 import './Resources.css';
+import { useMst } from '../../store/root';
+import { useObserver } from 'mobx-react';
+import { ITag } from '../../store/category';
+import { IResource } from '../../store/resource';
+
+export const resourceName = (name: string, displayName: string) => {
+  return displayName === '' ? (
+    <span style={{ fontFamily: 'courier, monospace' }}>{name}</span>
+  ) : (
+    <span>{displayName}</span>
+  );
+};
 
 const Resources = () => {
   const [pageNumber, setPageNumber] = React.useState(1);
@@ -25,7 +44,9 @@ const Resources = () => {
   ) => {
     setPerPage(perpage);
   };
-  return (
+  const { resources } = useMst();
+
+  return useObserver(() => (
     <React.Fragment>
       <Grid>
         <GridItem>
@@ -39,11 +60,68 @@ const Resources = () => {
           />
 
           <Gallery hasGutter>
-            {Array.apply(0, Array(20)).map((x, i) => (
-              <GalleryItem key={i}>
-                <Link to="/resource/name">
+            {resources.filteredResources.map((resource: IResource, r: number) => (
+              <GalleryItem key={r} style={{ margin: 'auto' }}>
+                <Link
+                  to={'/details?' + resources.filteredResources[r].name}
+                  style={{ textDecoration: 'none' }}
+                >
                   <Card isHoverable className="hub-resource-card">
-                    <CardBody>Resources</CardBody>
+                    <CardHeader>
+                      <Grid>
+                        <GridItem span={7}>
+                          <BuildIcon />
+                        </GridItem>
+
+                        <GridItem span={5}>
+                          <UserIcon />
+                        </GridItem>
+                      </Grid>
+
+                      <CardActions>
+                        <StarIcon />
+                        <TextContent className="text">
+                          {resources.filteredResources[r].rating}
+                        </TextContent>
+                      </CardActions>
+                    </CardHeader>
+
+                    <CardTitle>
+                      <Grid>
+                        <GridItem span={10}>
+                          <span className="hub-resource-name">
+                            {resourceName(
+                              resources.filteredResources[r].name,
+                              resources.filteredResources[r].latestVersion.displayName
+                            )}
+                          </span>
+                        </GridItem>
+
+                        <GridItem span={2}>
+                          <span className="hub-resource-version">
+                            v{resources.filteredResources[r].latestVersion.version}
+                          </span>
+                        </GridItem>
+                      </Grid>
+                    </CardTitle>
+
+                    <CardBody className="hub-resource-body">
+                      {resources.filteredResources[r].latestVersion.description}
+                    </CardBody>
+
+                    <CardFooter>
+                      <TextContent className="hub-resource-updatedAt">
+                        Updated {resources.filteredResources[r].latestVersion.updatedAt.fromNow()}
+                      </TextContent>
+
+                      <div style={{ height: '2.5em' }}>
+                        {resources.filteredResources[r].tags.slice(0, 3).map((tag: ITag) => (
+                          <Badge className="hub-tags" key={`badge-${tag.id}`}>
+                            {tag.name}
+                          </Badge>
+                        ))}
+                      </div>
+                    </CardFooter>
                   </Card>
                 </Link>
               </GalleryItem>
@@ -52,7 +130,7 @@ const Resources = () => {
         </GridItem>
       </Grid>
     </React.Fragment>
-  );
+  ));
 };
 
 export default Resources;
