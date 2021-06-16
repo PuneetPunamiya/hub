698a4fd2fc9796a2201903fafc034dc0e6db2d0e
PuneetPunamiya <puneetpunamiya@gmail.com>
2021-06-16 11:57:05 +0530

    Adds interactive mode for `get` command
       - This patch interactively ask the users to select
    
            - Catalog first
            - Resources in the catalog selected
            - All Versions of the resource selected
    
       - If there is only one catalog or version then
         it will be auto-selected
    
       - User can also get the manifest of the resource
         if the user know the catalog name, resource name
         and version of the resource by
    
       `tkn hub get task <task-name> --version <version-number> --from catalog <catalog-name>`
    
    Signed-off-by: Puneet Punamiya <ppunamiy@redhat.com>

--- api/pkg/cli/cmd/get/get_test.go
+++ api/pkg/cli/cmd/get/get_test.go
@@ -22,22 +22,39 @@ import (
 	"github.com/stretchr/testify/assert"
 	res "github.com/tektoncd/hub/api/gen/resource"
 	"github.com/tektoncd/hub/api/pkg/cli/test"
+	cat "github.com/tektoncd/hub/api/v1/gen/catalog"
 	goa "goa.design/goa/v3/pkg"
 	"gopkg.in/h2non/gock.v1"
 	"gotest.tools/v3/golden"
 )
 
-var want string = `
-Get a Abc of name 'foo':
+type InfoOptions struct {
+	ResId      int
+	Name       string
+	Kind       string
+	Catalog    string
+	Version    string
+	Resource   *res.ResourceData
+	VersionArr *res.Versions
+}
 
-    tkn hub get abc foo
+var catalogs = &cat.ListResult{
+	Data: []*cat.Catalog{
+		{
+			ID:   1,
+			Name: "tekton",
+			Type: "community",
+			URL:  "https://github.com/tektoncd/catalog",
+		},
+		{
+			ID:   2,
+			Name: "fruit",
+			Type: "community",
+			URL:  "https://github.com/fruit/catalog",
+		},
+	},
+}
 
-or
-
-Get a Abc of name 'foo' of version '0.3':
-
-    tkn hub get abc foo --version 0.3
-`
 var pipelineRes = &res.ResourceData{
 	ID:   1,
 	Name: "apple",
@@ -59,20 +76,20 @@ var pipelineRes = &res.ResourceData{
 		UpdatedAt:           "2020-01-01 12:00:00 +0000 UTC",
 	},
 	Tags: []*res.Tag{
-		&res.Tag{
+		{
 			ID:   3,
 			Name: "fruit",
 		},
 	},
 	Versions: []*res.ResourceVersionData{
-		&res.ResourceVersionData{
+		{
 			ID:      11,
 			Version: "0.1",
 		},
 	},
 }
 
-var pipelineResWithVersion = &res.ResourceVersionData{
+var pipelineResWithLatestVersion = &res.ResourceVersionData{
 	ID:                  11,
 	Version:             "0.3",
 	DisplayName:         "mango",
@@ -92,12 +109,118 @@ var pipelineResWithVersion = &res.ResourceVersionData{
 		},
 		Rating: 4.3,
 		Tags: []*res.Tag{
-			&res.Tag{
+			{
 				ID:   3,
 				Name: "fruit",
 			},
 		},
 	},
+}
+
+var pipelineResWithOldVersion = &res.ResourceVersionData{
+	ID:                  11,
+	Version:             "0.2",
+	DisplayName:         "mango",
+	Description:         "v0.3 of Pipeline mango",
+	MinPipelinesVersion: "0.12",
+	RawURL:              "http://raw.github.url/mango/0.2/mango.yaml",
+	WebURL:              "http://web.github.com/mango/0.2/mango.yaml",
+	UpdatedAt:           "2020-01-01 12:00:00 +0000 UTC",
+	Resource: &res.ResourceData{
+		ID:   7,
+		Name: "foo",
+		Kind: "Pipeline",
+		Catalog: &res.Catalog{
+			ID:   1,
+			Name: "fruit",
+			Type: "community",
+		},
+		Rating: 4.3,
+		Tags: []*res.Tag{
+			{
+				ID:   3,
+				Name: "fruit",
+			},
+		},
+	},
+}
+
+var ver = &res.Versions{
+	Latest: &res.ResourceVersionData{
+		ID:      11,
+		Version: "0.3",
+		RawURL:  "http://raw.github.url/mango/0.3/mango.yaml",
+		WebURL:  "http://web.github.com/mango/0.3/mango.yaml",
+	},
+	Versions: []*res.ResourceVersionData{
+		{
+			ID:      11,
+			Version: "0.3",
+			RawURL:  "http://raw.github.url/mango/0.3/mango.yaml",
+			WebURL:  "http://web.github.com/mango/0.3/mango.yaml",
+		},
+		{
+			ID:      10,
+			Version: "0.2",
+			RawURL:  "http://raw.github.url/mango/0.2/mango.yaml",
+			WebURL:  "http://web.github.com/mango/0.2/mango.yaml",
+		},
+	},
+}
+
+var want string = `
+Get a Abc of name 'foo':
+
+    tkn hub get abc foo
+
+or
+
+Get a Abc of name 'foo' of version '0.3':
+
+    tkn hub get abc foo --version 0.3
+`
+
+func mockApi(io InfoOptions, resourceWithVersion *res.ResourceVersionData) {
+
+	// Get all catalogs
+	gock.New(test.API).
+		Get("/v1/catalogs").
+		Reply(200).
+		JSON(&catalogs)
+
+	// Get all resources with catalog as Fruit
+	rArr := &res.Resources{Data: res.ResourceDataCollection{io.Resource}}
+	resources := res.NewViewedResources(rArr, "withoutVersion")
+	gock.New(test.API).
+		Get("/query").
+		MatchParam("catalogs", io.Catalog).
+		Reply(200).
+		JSON(&resources.Projected)
+
+	// Get ResourceId in order to get all versions of resource
+	rVer := &res.ResourceVersion{Data: resourceWithVersion}
+	resWithVersion := res.NewViewedResourceVersion(rVer, "default")
+
+	resInfo := fmt.Sprintf("%s/%s/%s", io.Catalog, io.Kind, io.Name)
+	gock.New(test.API).
+		Get("/resource/" + resInfo).
+		Reply(200).
+		JSON(&resWithVersion.Projected)
+
+	// Get all versios of the resource
+	v := &res.ResourceVersions{Data: io.VersionArr}
+	version := res.NewViewedResourceVersions(v, "min")
+
+	vArr := fmt.Sprintf("%d/versions", io.ResId)
+	gock.New(test.API).
+		Get("/resource/" + vArr).
+		Reply(200).
+		JSON(&version.Projected)
+
+	gock.New(test.API).
+		Get("/resource/" + resInfo + "/" + io.Version).
+		Reply(200).
+		JSON(&resWithVersion.Projected)
 }
 
 func TestValidate(t *testing.T) {
@@ -122,50 +245,20 @@ func TestValidate_ErrorCase(t *testing.T) {
 	assert.EqualError(t, err, "invalid value \"abc\" set for option version. valid eg. 0.1, 1.2.1")
 }
 
-func TestGet_WithoutVersion(t *testing.T) {
+func TestGetResource_WithNewVersion(t *testing.T) {
 	cli := test.NewCLI()
 
 	defer gock.Off()
 
-	resource := &res.Resource{Data: pipelineRes}
-	res := res.NewViewedResource(resource, "default")
-	gock.New(test.API).
-		Get("/resource/fruit/pipeline/apple").
-		Reply(200).
-		JSON(&res.Projected)
-
-	gock.New("http://raw.github.url").
-		Get("/apple/0.1/apple.yaml").
-		Reply(200).
-		File("./testdata/pipeline-apple-v0.1.yaml")
-
-	buf := new(bytes.Buffer)
-	cli.SetStream(buf, buf)
-
-	opt := &options{
-		cli:  cli,
-		kind: "pipeline",
-		args: []string{"apple"},
-		from: "fruit",
-	}
-
-	err := opt.run()
-	assert.NoError(t, err)
-	golden.Assert(t, buf.String(), fmt.Sprintf("%s.golden", t.Name()))
-	assert.Equal(t, gock.IsDone(), true)
-}
-
-func TestGet_WithVersion(t *testing.T) {
-	cli := test.NewCLI()
-
-	defer gock.Off()
-
-	resVersion := &res.ResourceVersion{Data: pipelineResWithVersion}
-	res := res.NewViewedResourceVersion(resVersion, "default")
-	gock.New(test.API).
-		Get("/resource/fruit/pipeline/mango/0.3").
-		Reply(200).
-		JSON(&res.Projected)
+	mockApi(InfoOptions{
+		ResId:      12,
+		Name:       "mango",
+		Kind:       "pipeline",
+		Catalog:    "fruit",
+		Version:    "0.3",
+		Resource:   pipelineRes,
+		VersionArr: ver,
+	}, pipelineResWithLatestVersion)
 
 	gock.New("http://raw.github.url").
 		Get("/mango/0.3/mango.yaml").
@@ -186,7 +279,44 @@ func TestGet_WithVersion(t *testing.T) {
 	err := opt.run()
 	assert.NoError(t, err)
 	golden.Assert(t, buf.String(), fmt.Sprintf("%s.golden", t.Name()))
-	assert.Equal(t, gock.IsDone(), true)
+	assert.Equal(t, gock.IsDone(), false)
+}
+
+func TestGetResource_WithOldVersion(t *testing.T) {
+	cli := test.NewCLI()
+
+	defer gock.Off()
+
+	mockApi(InfoOptions{
+		ResId:      12,
+		Name:       "mango",
+		Kind:       "pipeline",
+		Catalog:    "fruit",
+		Version:    "0.2",
+		Resource:   pipelineRes,
+		VersionArr: ver,
+	}, pipelineResWithOldVersion)
+
+	gock.New("http://raw.github.url").
+		Get("/mango/0.2/mango.yaml").
+		Reply(200).
+		File("./testdata/pipeline-mango-v0.2.yaml")
+
+	buf := new(bytes.Buffer)
+	cli.SetStream(buf, buf)
+
+	opt := &options{
+		cli:     cli,
+		kind:    "pipeline",
+		args:    []string{"mango"},
+		from:    "fruit",
+		version: "0.2",
+	}
+
+	err := opt.run()
+	assert.NoError(t, err)
+	golden.Assert(t, buf.String(), fmt.Sprintf("%s.golden", t.Name()))
+	assert.Equal(t, gock.IsDone(), false)
 }
 
 func TestGet_ResourceNotFound(t *testing.T) {
