6079ca64a7896484ef888ec25ff2fa9463efc544
pratap0007 <shverma@redhat.com>
2021-07-02 20:05:07 +0530

    Updates the Response of v1/API's to include deprecation info for a version
    - If a version/latest version of resource is deprecated then it adds
      deprecation info to respective version info in the response as
      deprecated:"true"` otherwise it doesn't add
    
    - Following api's response would change
                - v1/resource/version/<versionID>
                - v1/resource/version/<version>
                - v1/resources or /query (if latest version is deprecated)
    
    Signed-off-by: Shiv Verma <shverma@redhat.com>

--- api/v1/service/resource/resource.go
+++ api/v1/service/resource/resource.go
@@ -307,6 +307,12 @@ func initResource(r model.Resource) *resource.ResourceData {
 		UpdatedAt:           lv.ModifiedAt.UTC().Format(time.RFC3339),
 		Platforms:           platforms,
 	}
+
+	// If latest version is deprecated then add it only
+	if lv.Deprecated == "true" {
+		res.LatestVersion.Deprecated = &lv.Deprecated
+	}
+
 	res.Tags = []*resource.Tag{}
 	for _, tag := range r.Tags {
 		res.Tags = append(res.Tags, &resource.Tag{
@@ -422,6 +428,11 @@ func versionInfoFromResource(r model.Resource) *resource.ResourceVersion {
 		UpdatedAt:           v.ModifiedAt.UTC().Format(time.RFC3339),
 		Resource:            res,
 		Platforms:           verPlatforms,
+	}
+
+	// If version is deprecated then add it only
+	if v.Deprecated == "true" {
+		ver.Deprecated = &v.Deprecated
 	}
 
 	return &resource.ResourceVersion{Data: ver}
